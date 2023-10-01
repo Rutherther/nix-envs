9468957c2197bbb0de9763db49e836557cc40577
Rutherther <rutherther@proton.me>
2023-10-01 09:16:45 +0200

    feat: add Matlab program

---
+++ programs/matlab/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Matlab.";
+
+  inputs = {
+    nix-matlab = {
+      url = "gitlab:doronbehar/nix-matlab";
+    };
+  };
+  outputs = { self, nixpkgs, nix-matlab }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.matlab = nix-matlab.packages.x86_64-linux.matlab;
+    packages.x86_64-linux.matlab-mlint = nix-matlab.packages.x86_64-linux.matlab-mlint;
+    packages.x86_64-linux.matlab-mex = nix-matlab.packages.x86_64-linux.matlab-mex;
+    packages.x86_64-linux.octave = pkgs.octaveFull;
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = (with nix-matlab.packages.x86_64-linux; [
+        matlab
+        matlab-mlint
+        matlab-mex
+        pkgs.octaveFull
+      ]);
+      shellHook = nix-matlab.shellHooksCommon;
+    };
+  };
+}
